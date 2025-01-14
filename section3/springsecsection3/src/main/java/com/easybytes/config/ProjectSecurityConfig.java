a0bdeda26812f5b68f5d5b2d148bfef252642b01
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-01-14 23:04:59 +0900

    시큐리티 사용자 테스트

--- section3/springsecsection3/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ section3/springsecsection3/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.easybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,14 +17,19 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        /*http.formLogin(withDefaults());*/
-        /*http.httpBasic(withDefaults());*/
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices", "/contact", "/error").permitAll());
-        http.formLogin((flc) -> flc.disable());
-        http.httpBasic((hbc) -> hbc.disable());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =  User.withUsername("user").password("{noop}1234").authorities("read").build();
+        UserDetails admin =  User.withUsername("admin").password("{noop}4321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
