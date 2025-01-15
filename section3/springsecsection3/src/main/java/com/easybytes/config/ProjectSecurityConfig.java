74f545f765db3b175f84f063891b9820bf65b06c
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-01-15 22:43:28 +0900

    Password Encoder 테스트

--- section3/springsecsection3/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ section3/springsecsection3/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -2,12 +2,16 @@ package com.easybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
@@ -15,8 +19,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices", "/contact", "/error").permitAll());
@@ -27,9 +29,25 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =  User.withUsername("user").password("{noop}1234").authorities("read").build();
-        UserDetails admin =  User.withUsername("admin").password("{noop}4321").authorities("admin").build();
+        UserDetails user =  User.withUsername("user").password("{noop}easybytes@1234").authorities("read").build();
+        UserDetails admin =  User.withUsername("admin")
+                                .password("{bcrypt}$2a$12$XfzZGP8/4seEOSBHCtxSM.RiV9s2oZx2ki5VXA1TPGs3z4WwxkeMC")
+                                .authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * 스프링 시큐리티 6.3
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 
 }
