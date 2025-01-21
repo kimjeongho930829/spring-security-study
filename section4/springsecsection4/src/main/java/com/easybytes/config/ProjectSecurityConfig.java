7417b022740d233e4f9f0e19bf8ae2a5ee3fe7ce
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-01-21 17:56:19 +0900

    UserDetailsService 구현

--- section4/springsecsection4/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ section4/springsecsection4/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -27,10 +27,10 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
