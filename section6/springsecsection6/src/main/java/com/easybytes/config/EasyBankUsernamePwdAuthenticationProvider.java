b627044e442cdc400e35d8cc8a1a8603a01e4af3
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-01-25 23:25:34 +0900

    커스텀 AuthenticationProvider 테스트

---
+++ section6/springsecsection6/src/main/java/com/easybytes/config/EasyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,37 @@
+package com.easybytes.config;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.jaxb.internal.stax.MappingEventReader;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EasyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+        } else {
+            throw new MappingEventReader.BadVersionException("Invalid password");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
