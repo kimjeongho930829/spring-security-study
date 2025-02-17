52cef8ec0928f00ea1562c8b9da825f7993b044d
kimjeongho930829 <kimjeongho930829@gmail.com>
2025-02-17 21:08:50 +0900

    Spring MVC 로그아웃 테스트

--- section7/easyschool-end/src/main/java/com/easybytes/easyschool/controller/LoginController.java
+++ section7/easyschool-end/src/main/java/com/easybytes/easyschool/controller/LoginController.java
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET})
-    public String displayLoginPage(@RequestParam(value = "error", required = false)String error,Model model) {
+    public String displayLoginPage(@RequestParam(value = "error", required = false)String error,
+                                   @RequestParam(value = "logout", required = false)String logout,Model model) {
         String errorMessge = null;
         if(error != null) {
             errorMessge = "Username or Password is incorrect!";
+        }
+        if(logout != null) {
+            errorMessge = "You have been successfully logged out!";
         }
         model.addAttribute("errorMessge", errorMessge);
         return "login.html";
